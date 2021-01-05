b718477c6937bfb38f9b99034cd0f2761b5e866e
wecko1980 <76854446+wecko1980@users.noreply.github.com>
2021-01-05 01:42:24 +0100

    much has been done this evening :-)

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-	
-})
 input.onButtonPressed(Button.A, function () {
     Cursor.change(LedSpriteProperty.Y, VerticalMove)
     if (Cursor.get(LedSpriteProperty.Y) == 0) {
@@ -10,6 +7,48 @@ input.onButtonPressed(Button.A, function () {
         VerticalMove = -1
     }
 })
+function ShipSink () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # . # . #
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . # # . .
+        . # # # #
+        . # # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # .
+        . # # . .
+        . # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # # .
+        . # # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # # .
+        `)
+    basic.pause(200)
+}
 input.onButtonPressed(Button.AB, function () {
     game.pause()
     led.plotBrightness(Ship1X, Ship1Y, 10)
@@ -32,29 +71,54 @@ input.onButtonPressed(Button.B, function () {
 radio.onReceivedValue(function (name, value) {
     if (name == "1ROW" && value == FleetX[0]) {
         music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
-        game.removeLife(1)
-    } else {
+        ShipsRemaining += -1
+        ShipSink()
+    }
+    if (name == "1ROW" && value != FleetX[0]) {
         music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.OnceInBackground)
     }
     if (name == "2ROW" && value == FleetX[1]) {
         music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
-    } else {
+        ShipsRemaining += -1
+        ShipSink()
+    }
+    if (name == "2ROW" && value != FleetX[1]) {
         music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.OnceInBackground)
     }
     if (name == "3ROW" && value == FleetX[2]) {
         music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
-    } else {
+    }
+    if (name == "3ROW" && value != FleetX[2]) {
         music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.OnceInBackground)
     }
     if (name == "4ROW" && value == FleetX[3]) {
         music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
-    } else {
+    }
+    if (name == "4ROW" && value != FleetX[3]) {
         music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.OnceInBackground)
     }
     if (name == "5ROW" && value == FleetX[4]) {
         music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
-    } else {
+    }
+    if (name == "5ROW" && value != FleetX[4]) {
         music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.OnceInBackground)
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (Cursor.get(LedSpriteProperty.Y) == 0) {
+        radio.sendValue("1ROW", Cursor.get(LedSpriteProperty.X))
+    }
+    if (Cursor.get(LedSpriteProperty.Y) == 1) {
+        radio.sendValue("2ROW", Cursor.get(LedSpriteProperty.X))
+    }
+    if (Cursor.get(LedSpriteProperty.Y) == 2) {
+        radio.sendValue("3ROW", Cursor.get(LedSpriteProperty.X))
+    }
+    if (Cursor.get(LedSpriteProperty.Y) == 3) {
+        radio.sendValue("4ROW", Cursor.get(LedSpriteProperty.X))
+    }
+    if (Cursor.get(LedSpriteProperty.Y) == 4) {
+        radio.sendValue("5ROW", Cursor.get(LedSpriteProperty.X))
     }
 })
 let Cursor: game.LedSprite = null
@@ -72,9 +136,9 @@ let Ship1X = 0
 let HorizontalMove = 0
 let VerticalMove = 0
 radio.setGroup(1)
-let Player1 = randint(0, 4)
 VerticalMove = 1
 HorizontalMove = 1
+let ShipsRemaining = 5
 Ship1X = randint(0, 4)
 Ship2X = randint(0, 4)
 Ship3X = randint(0, 4)
@@ -89,25 +153,3 @@ FleetX = [Ship1X, Ship2X, Ship3X, Ship4X, Ship5X]
 let FleetY = [Ship1Y, Ship2Y, Ship3Y, Ship4Y, Ship5Y]
 Cursor = game.createSprite(0, 0)
 Cursor.set(LedSpriteProperty.Blink, 600)
-basic.forever(function () {
-	
-})
-basic.forever(function () {
-    while (input.logoIsPressed()) {
-        if (Cursor.get(LedSpriteProperty.Y) == 0) {
-            radio.sendValue("1ROW", Cursor.get(LedSpriteProperty.X))
-        }
-        if (Cursor.get(LedSpriteProperty.Y) == 1) {
-            radio.sendValue("2ROW", Cursor.get(LedSpriteProperty.X))
-        }
-        if (Cursor.get(LedSpriteProperty.Y) == 2) {
-            radio.sendValue("3ROW", Cursor.get(LedSpriteProperty.X))
-        }
-        if (Cursor.get(LedSpriteProperty.Y) == 3) {
-            radio.sendValue("4ROW", Cursor.get(LedSpriteProperty.X))
-        }
-        if (Cursor.get(LedSpriteProperty.Y) == 4) {
-            radio.sendValue("5ROW", Cursor.get(LedSpriteProperty.X))
-        }
-    }
-})
